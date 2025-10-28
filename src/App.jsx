6083c4329592d3ef642c3d72e29c6e7b82903a23
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-10-28 13:23:59 +0900

    :art: Design: 런칭 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import AppRouter from './routes/AppRouter'
+import LaunchingPage from './pages/exception/LaunchingPage'
 
 function App() {
-  return <AppRouter />
+  //return <AppRouter />
+  return <LaunchingPage />
 }
 
 export default App
